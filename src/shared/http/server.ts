c8ccae2cbe725428e983b11b462475aa522d6803
Felipe Araujo <felipe.araujo@capgemini.com>
2023-03-24 13:15:46 -0300

    error class configuration and typeorm installation

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,8 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
-import AppError from '@shared/errors/AppError';
+import AppError from './../errors/AppError';
 
 const app = express();
 const port = 3333;
