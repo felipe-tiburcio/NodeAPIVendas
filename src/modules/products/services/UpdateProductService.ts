2ebeac091acef1de800967b378358604c0edd731
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2023-03-27 15:47:55 -0300

    update product service and little modification in other services

--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -5,17 +5,38 @@ import AppError from './../../../shared/errors/AppError';
 
 interface IRequest {
   id: string;
+  name: string;
+  price: number;
+  quantity: number;
 }
 
 class ShowProductService {
-  public async execute({ id }: IRequest): Promise<Product> {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
     const productsRepository = getCustomRepository(ProductRepository);
+    const productExists = await productsRepository.findByName(name);
 
     const product = await productsRepository.findOne(id);
 
     if (!product) {
       throw new AppError('Product not found.');
     }
+
+    if (productExists && name !== product.name) {
+      throw new AppError(
+        "Can't update because a product with same name already exists.",
+      );
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
 
     return product;
   }
